344bdf755ec755127b1897a450db388f59d779c0
M1nistry <Ministry@live.com.au>
2013-03-06 05:36:07 +1100

    Create results.vb

---
+++ results.vb
@@ -0,0 +1,7 @@
+Public Class Results
+
+    Private Sub okButton_Click(sender As Object, e As EventArgs) Handles okButton.Click
+        valueBox.Text = ""
+        Me.Dispose()
+    End Sub
+End Class
